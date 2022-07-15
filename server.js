7e1657b78a3ae324eb1539a747fc866b9f07642e
VandelayUtd <drewhadley.guitar@gmail.com>
2022-07-15 16:29:12 -0400

    setup boilerplate html

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log('here')
-    res.render('index')
+    res.render('index', {text: 'World'})
 })
 
 
