8ae292c5b5861886a9aa1c3ae136169b9b309590
VandelayUtd <drewhadley.guitar@gmail.com>
2022-07-16 12:34:40 -0400

    added /users and /users/new routes

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     console.log('here')
-    res.render('index', {text: 'World'})
+    res.render('index', { text: 'World' })
+})
+
+app.get('/users', (req, res) => {
+    res.send('user list')
+})
+
+app.get('/users/new', (req, res) => {
+    res.send('user new form')
 })
 
 
